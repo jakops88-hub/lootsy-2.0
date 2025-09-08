b897a9e6000254ec8169b268fd74a904b04e3a44
jakops88-hub <jakops88@gmail.com>
2025-09-08 12:09:13 +0200

    Update feeds_template.sql

--- lootsy-2.2-pro/feeds_template.sql
+++ lootsy-2.2-pro/feeds_template.sql
@@ -1,7 +1,7 @@
 -- KLIStra IN feed-URL: ersätt YOUR_FEED_URL med din nätverkslänk (CSV/TSV/JSON)
 -- format: 'csv' eller 'tsv' eller 'json'
 insert into feeds (merchant_name, url, format, default_tags, active) values
-('Amazon SE', 'YOUR_FEED_URL', 'csv', '{tech,kampanj}', true),
+('Amazon SE', 'https://amzn.to/4m8ISp1', 'csv', '{tech,kampanj}', true),
 ('CDON', 'YOUR_FEED_URL', 'csv', '{tech,hem}', true),
 ('Webhallen', 'YOUR_FEED_URL', 'csv', '{tech,gaming}', true),
 ('Inet', 'YOUR_FEED_URL', 'csv', '{tech,gaming}', true),
