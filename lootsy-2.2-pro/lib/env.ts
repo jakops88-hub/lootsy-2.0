bd7014ed20e87acf69a6d54169831a67bcef1962
jakops88-hub <jakops88@gmail.com>
2025-09-08 13:56:20 +0200

    Add files via upload

--- lootsy-2.2-pro/lib/env.ts
+++ lootsy-2.2-pro/lib/env.ts
@@ -5,8 +5,4 @@ export const env = {
   adminEmail: process.env.ADMIN_EMAIL || "",
   adminSecret: process.env.ADMIN_SECRET || "",
   appName: process.env.NEXT_PUBLIC_APP_NAME || "Lootsy",
-  stripePK: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "",
-  stripeSK: process.env.STRIPE_SECRET_KEY || "",
-  stripePriceId: process.env.STRIPE_PRICE_ID_PREMIUM || "",
-  stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET || "",
 };
